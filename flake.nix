a683d40417e131007b012fe66db4a5c3a4914bcd
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-09-30 22:38:06 +0200

    Add mapMidgardOverlay

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
-  outputs = { self }: {
+  outputs = { self }:
+  let
+    inherit (builtins) mapAttrs;
+  in {
 
-    lib = {
+    lib = rec {
       midgardOverlay = overlay: (final: prev: { midgard = (prev.midgard or { }) // (overlay final prev); });
+      mapMidgardOverlay = mapAttrs (_: overlay: midgardOverlay overlay);
     };
 
   };
